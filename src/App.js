ead6b00c98fa7118efc9359da3ba4fdb420a696a
Shaun Stephenson <shaunfitzgarald@gmail.com>
2023-10-12 19:25:43 -0700

    Deployed Website

--- src/App.js
+++ src/App.js
@@ -1,36 +1,86 @@
-import React, { useState } from 'react'
-import Square from './components/Square'
-import './App.css'
+import React, { useState } from 'react';
+import Square from './components/Square';
+import './App.css';
 
 const App = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  
-    // const handleClick = (selectedIndex) => {
-    // let userClick = squares.map((index) => 
-    // index + 1
-    const handleClick = (selectedIndex) => {
-      if(squares[selectedIndex] === null) {
-        squares[selectedIndex]="❌"
-        setSquares([...squares])
+  // Initialize the state with an array to represent the game board.
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  // Define a state to keep track of the current player (X or O).
+  const [isXNext, setIsXNext] = useState(true);
+
+  const handleClick = (selectedIndex) => {
+    // Check if the square is already filled or if the game is over.
+    if (squares[selectedIndex] || calculateWinner(squares)) {
+      return;
+    }
+
+    // Create a copy of the squares array to avoid mutating state directly.
+    const newSquares = squares.slice();
+    // Set the value of the clicked square based on the current player.
+    newSquares[selectedIndex] = isXNext ? "❌" : "⭕️";
+
+    // Update the state with the new squares array and toggle the player.
+    setSquares(newSquares);
+    setIsXNext(!isXNext);
+  };
+  const resetGame = () => {
+    // Reset the game board and current player when the "Reset Game" button is clicked.
+    setSquares(Array(9).fill(null));
+    setIsXNext(true)
+  }
+  // Function to determine the winner of the game.
+  const calculateWinner = (squares) => {
+    // Define winning combinations.
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < winningCombinations.length; i++) {
+      const [a, b, c] = winningCombinations[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
       }
     }
-    // )
-    // if (userClick % 2 === 0) {
-    //   squares[selectedIndex]="❌"
-    //   setSquares([...squares])
-    // }
-    // else {
-    //   squares[selectedIndex]="⭕️"
-    //   setSquares([...squares])
-    // }
-    // }
-    
-    return (
-    <>
-      <h1>Tic Tac Toe</h1>
-      <Square squares={squares} handleClick={handleClick}/>
-    </>
-  )
-}
 
-export default App
+    // If there's no winner, check if all squares are filled (a draw).
+    if (squares.every((square) => square !== null)) {
+      return 'Draw';
+    }
+
+    return null;
+  };
+
+  // Determine the status of the game.
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${isXNext ? '❌' : '⭕️'}`;
+  }
+
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe</h1>
+      <div className="status">{status}</div>
+      <div className="board">
+        {squares.map((value, index) => (
+          <Square key={index} value={value} onClick={() => handleClick(index)} />
+        ))}
+      </div>
+      <button className="reset-button" onClick={resetGame}>
+        Reset Game
+      </button>
+    </div>
+  );
+};
+
+export default App;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,24 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-const Square = ({squares, handleClick}) => {
-  console.log("string", squares)
-return (
-<>
-<div className="grid">
-{squares.map((value, index) => {
-  console.log("value", index)
- return (
-  <div 
-  className="square" 
-  key={index}
-  onClick={() => handleClick(index)}
- >
-  {value} 
- </div>
- )
- })}
-  </div>
-</>)
-}
-export default Square
+const Square = ({ value, onClick }) => {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+};
 
+export default Square;
